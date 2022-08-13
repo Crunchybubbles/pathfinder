bb5c451d0897cf1ae29e3d57e56e62464ace0388
jasper <jasperspafford@gmail.com>
2022-08-13 16:03:51 -0700

    blazingly fast

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use serde_json::Value;
 use std::fs;
 use std::io;
 use std::collections::HashMap;
+use std::iter::Inspect;
+use std::time;
 use std::time::Instant;
 use ethers::types::{Address, U256};
 use serde::{Serialize, Deserialize};
@@ -31,18 +33,18 @@ struct _UniV3Pool {
     ticks: Vec<_Tick>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 struct Tick {
     tick_idx: i64,
     liquidity_net: U256,
 }
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 struct Token {
     id: Address,
     symbol: String,
     decimals: u64,
 }
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 struct UniV3Pool {
     id: Address,
     token0: Token,
@@ -78,12 +80,20 @@ impl UniV3Pool {
 	
     }
 
+    fn token_out(&self, token_in: &Address) -> &Address {
+	if token_in == &self.token0.id {
+	    return &self.token1.id;
+	} else {
+	    return &self.token0.id;
+	}
+    }
+
 }
 #[derive(Debug, Deserialize)]
 struct _TokenV2 {
     id: String,
 }
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 struct TokenV2 {
     id: Address,
 }
@@ -95,7 +105,7 @@ struct _UniV2Pool {
     token1: _TokenV2,  
 }
 
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 struct UniV2Pool {
     id: Address,
     token0: TokenV2,
@@ -115,12 +125,105 @@ impl UniV2Pool {
 	UniV2Pool{id, token0, token1}
 
     }
+
+    fn token_out(&self, token_in: &Address) -> &Address {
+	if token_in == &self.token0.id {
+	    return &self.token1.id;
+	} else {
+	    return &self.token0.id;
+	}
+    }
 }
 
-#[derive(Debug, Serialize, Deserialize)]    
+#[derive(Debug, Serialize, Deserialize, Clone)]    
 enum Pool {
     V3(UniV3Pool),
     V2(UniV2Pool),
+}
+
+impl Pool {
+    fn token_out(&self, token_in: &Address) -> &Address {
+	match self {
+	    Pool::V2(pooldata) => {
+		return pooldata.token_out(token_in)
+	    }
+
+	    Pool::V3(pooldata) => {
+		return pooldata.token_out(token_in)
+	    }
+	}
+    }
+
+    fn token0(&self) -> &Address {
+	match self {
+	    Pool::V2(pool) => {
+		return &pool.token0.id;
+	    }
+
+	    Pool::V3(pool) => {
+		return &pool.token0.id;
+	    }
+	}
+    }
+
+
+    fn token1(&self) -> &Address {
+	match self {
+	    Pool::V2(pool) => {
+		return &pool.token1.id;
+	    }
+
+	    Pool::V3(pool) => {
+		return &pool.token1.id;
+	    }
+	}
+    }
+
+    fn addr(&self) -> &Address {
+	match self {
+	    Pool::V2(pool) => {
+		return &pool.id;
+	    }
+
+	    Pool::V3(pool) => {
+		return &pool.id;
+	    }
+	}
+    }
+
+}
+
+struct Graph<T, G> {
+    nodes: Vec<Box<T>>,
+    edges: HashMap<G, Vec<Box<T>>>,
+}
+
+impl Graph<Pool, Address> {
+    fn new(pools: Vec<Pool>) -> Self {
+	let mut nodes: Vec<Box<Pool>> = Vec::new();
+	let mut edges: HashMap<Address, Vec<Box<Pool>>> = HashMap::new();
+	for pool in pools {
+	    let token0 = *pool.token0();
+	    let token1 = *pool.token1();
+	    let b = Box::new(pool);
+	    if let Some(stuff) = edges.get_mut(&token0) {
+		stuff.push(b.clone());
+	    } else {
+		edges.insert(token0, vec![b.clone()]);
+	    }
+
+	    if let Some(stuff) = edges.get_mut(&token1) {
+		stuff.push(b.clone());
+	    } else {
+		edges.insert(token1, vec![b.clone()]);
+	    }
+	    nodes.push(b);
+    
+	}
+
+	return Graph{nodes, edges};
+    }
+    
 }
 
 fn uni3() -> io::Result<Vec<UniV3Pool>> {
@@ -145,102 +248,64 @@ fn load_pools() -> io::Result<Vec<Pool>> {
     let uni3 = uni3().unwrap();
     let uni2pools = uni2("uni2data.txt").unwrap();
     let sushi = uni2("sushipools.txt").unwrap();
-
     let mut pools: Vec<Pool> = Vec::new();
-
     for pool in uni3 {
 	let p = Pool::V3(pool);
 	pools.push(p);
+
     }
 
     for pool in uni2pools {
 	let p = Pool::V2(pool);
 	pools.push(p);
+
+    
     }
 
     for pool in sushi {
 	let p = Pool::V2(pool);
 	pools.push(p);
-    }
 
+    
+    }
     
     return Ok(pools);    
 }
 
-fn market_map(pools: &Vec<Pool>) -> HashMap<Address, Vec<Address>> {
-    let mut mm: HashMap<Address, Vec<Address>> = HashMap::new();
-    for pool in pools {
-	match pool {
-	    Pool::V2(pooldata) => {
-		if !mm.contains_key(&pooldata.token0.id) {
-		    let mut v: Vec<Address> = Vec::new();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token0.id, v);
-		} else {
-		    let mut v: Vec<Address> = mm.get(&pooldata.token0.id).unwrap().to_vec();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token0.id, v.to_vec());
-		}
-
-		if !mm.contains_key(&pooldata.token1.id) {
-		    let mut v: Vec<Address> = Vec::new();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token1.id, v);
-		} else {
-		    let mut v: Vec<Address> = mm.get(&pooldata.token1.id).unwrap().to_vec();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token1.id, v.to_vec());
-		}
-	    },
-	    Pool::V3(pooldata) => {
-		if !mm.contains_key(&pooldata.token0.id) {
-		    let mut v: Vec<Address> = Vec::new();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token0.id, v);
-		} else {
-		    let mut v: Vec<Address> = mm.get(&pooldata.token0.id).unwrap().to_vec();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token0.id, v.to_vec());
-		}
-
-		if !mm.contains_key(&pooldata.token1.id) {
-		    let mut v: Vec<Address> = Vec::new();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token1.id, v);
-		} else {
-		    let mut v: Vec<Address> = mm.get(&pooldata.token1.id).unwrap().to_vec();
-		    v.push(pooldata.id);
-		    mm.insert(pooldata.token1.id, v.to_vec());
-		}
-	    }
-	}
-	
-    }
-
-
-    return mm;
-}
-
-
-fn save_market_map() -> std::io::Result<()> {
-    let pools = load_pools().unwrap();
-    let mm = market_map(&pools);
-    let mut file = File::create("market_map.txt")?;
-    serde_json::to_writer(file, &mm)?;
-    Ok(())
-
-}
-
-fn load_market_map() -> std::io::Result<HashMap<Address, Vec<Address>>> {
-    let file = File::open("market_map.txt")?;
-    let reader = BufReader::new(file);
-    let mm: HashMap<Address, Vec<Address>> = serde_json::from_reader(reader)?;
-    Ok(mm)
-}
-
 
 fn main() {
-    let mm = load_market_map().unwrap();
-    let weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".parse::<Address>().unwrap();
-    println!("{:#?}", mm.get(&weth));
+    let pools = load_pools().unwrap();
+    //let now = Instant::now();
+    //let mm = market_map(&pools);
+    //let mm_time = now.elapsed();
+    let now = Instant::now();
+    //let pooldata = pools_to_map(pools);
+    let graph = Graph::new(pools);
+    let pd_time = now.elapsed();
+    
+    //println!("{}", mm_time.as_millis());
+    println!("{}", pd_time.as_millis());
+    //let weth = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2".parse::<Address>().unwrap();
+    //let yfi = "0x0bc529c00C6401aEF6D220BE8C6Ea1667F6Ad93e".parse::<Address>().unwrap();
+    println!("{}", graph.nodes.len());
+    // for node in graph.nodes {
+// 	match *node {
+// 	    Pool::V2(_) => {}
+// 	    Pool::V3(_) => {println!("im doing stuff")}
+// 	}
+// //	println!("{:#?}", *node);
+    //     }
+    for (token, pools) in graph.edges.iter() {
+
+	if pools.len() > 2 {
+	    println!("{}", token);
+	    println!("{}", pools.len());
+	    println!("");
+	}
+	
+
+    }
+    //let path = graph.find_path(yfi, weth);
+    //println!("{:?}", path);
+    // println!("{:#?}", mm.get(&weth));
 }
